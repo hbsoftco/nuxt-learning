15cc8020104a791213fc57c9d54c2581c618196d
hbsoftco <hossein.bajan@gmail.com>
2020-02-01 09:46:29 +0330

    Init nuxt

---
+++ layouts/inspireLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>i am hossen layout</div>
+</template>
+
+<script>
+export default {
+  name:"InspireLayout",
+  data() {
+    return {
+      title: 'Hossein Bajan'
+    }
+  }
+}
+</script>
